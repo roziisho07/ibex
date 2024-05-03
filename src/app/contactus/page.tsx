36365fc5f9518e672c2c3f591d1f429e9b92cce9
roziisho07 <roziisho@gmail.com>
2024-05-03 05:38:06 +0500

    added contact page

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,15 +1,13 @@
-import Contactuss from "../components/widget/contactuss"
-import Footer from "../components/widget/footer"
+import Contactuss from "../components/widget/contactuss";
+import Footer from "../components/widget/footer";
 
 export default function contactus() {
   return (
     <div>
-      <div
-        className="bg-cover bg-center h-[35rem] w-full "
-        style={{ backgroundImage: `url('/images/accomodationpage.jpg')` }}
-      ></div>
-      <Contactuss/>
-      <Footer/>
+      <div className="bg-cover bg-center h-auto pt-20  w-full ">
+        <Contactuss />
+      </div>
+      <Footer />
     </div>
   );
 }
